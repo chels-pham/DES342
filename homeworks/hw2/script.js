29de8834d981e411f313e78e962e798c68739f86
chels-pham <97709605+chels-pham@users.noreply.github.com>
2022-04-05 17:32:01 -0700

    add hw2

---
+++ homeworks/hw2/script.js
@@ -0,0 +1,39 @@
+$('h1').click(function () {
+  $('body').toggleClass('green')
+})
+
+$('.one').click(function () {
+  $('.panels').attr('src', 'images/panel-one.jpg')
+})
+
+$('.two').click(function () {
+  $('.panels').attr('src', 'images/panel-two.jpg')
+})
+
+$('.three').click(function () {
+  $('.panels').attr('src', 'images/panel-three.jpg')
+})
+
+$('.four').click(function () {
+  $('.panels').attr('src', 'images/panel-four.jpg')
+})
+
+$('.five').click(function () {
+  $('.panels').attr('src', 'images/panel-five.jpg')
+})
+
+$('.six').click(function () {
+  $('.panels').attr('src', 'images/panel-six.jpg')
+})
+
+$('.seven').click(function () {
+  $('.panels').attr('src', 'images/panel-seven.jpg')
+})
+
+$('.eight').click(function () {
+  $('.panels').attr('src', 'images/panel-eight.jpg')
+})
+
+$('.nine').click(function () {
+  $('.panels').attr('src', 'images/panel-nine.jpg')
+})
